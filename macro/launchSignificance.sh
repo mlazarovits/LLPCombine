7f11c35625f56f33502fa764781fa0dc5d7f496c
mlazarovits <mrlazarovits@gmail.com>
2026-04-20 17:44:47 -0500

    setting input datacard dir from cmd line

--- macro/launchSignificance.sh
+++ macro/launchSignificance.sh
@@ -2,8 +2,7 @@
 #dcdir=datacards_test1_MCStats
 #dcdir=datacards_2photon_prompt
 #dcdir=datacards_2photon_prompt4bin
-dcdir=datacards_PhoSV_2PhoPrompt_Hadronic_NonCompressed_Sensitivity
-
+dcdir=$1
 pushd ../
 #combineTool.py -M AsymptoticLimits  -d ${dcdir}/*/*.txt --there -n .limit --parallel 4
 combineTool.py -M Significance -t -1 --expectSignal=1 -d ${dcdir}/*/*.txt --there --parallel 4
