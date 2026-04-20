b48b8a8fb83461f6a23f857bb965b79f91608ef9
mlazarovits <mrlazarovits@gmail.com>
2026-04-20 18:18:57 -0500

    can launch launchSignificance.sh from base llpcombine dir

--- macro/launchSignificance.sh
+++ macro/launchSignificance.sh
@@ -3,8 +3,9 @@
 #dcdir=datacards_2photon_prompt
 #dcdir=datacards_2photon_prompt4bin
 dcdir=$1
-pushd ../
+
+#pushd ../
 #combineTool.py -M AsymptoticLimits  -d ${dcdir}/*/*.txt --there -n .limit --parallel 4
 combineTool.py -M Significance -t -1 --expectSignal=1 -d ${dcdir}/*/*.txt --there --parallel 4
 #combineTool.py -M Significance -t -1 --expectSignal=0 -d ${dcdir}/*/*.txt --there --parallel 4
-popd
+#popd
